a7d5b646f147d639194c9203101ef54f207e2aa2
rajneesh75 <rajneesh75@yahoo.com>
2025-11-21 11:41:55 +0530

    Merge remote-tracking branch 'origin/main'

--- nonfinetuned.py
+++ nonfinetuned.py
@@ -1,20 +1,14 @@
 import lamini
-lamini.api_key = "0cfda3d014bb341b4028278c035b2a8d9eb17475e3d4ae94712d310c541b696a"
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+lamini.api_key = os.getenv("LAMINI.API_KEY")
 
 
 llm = lamini.Lamini(
     model_name="meta-llama/Llama-2-7b-hf",
 )
 response = llm.generate("Tell me how to train my dog to sit.")
-
-
-#print(response['output'] if isinstance(response, dict) else response)
-
-#if isinstance(response, dict):
-#    print('dictionary output - {}'.format(response['output']))
-#else:
-#    print('non dictionary output - {}'.format(response))
-
-
-
 print('non dictionary output - {}'.format(response))
