8aaaaf9930afabbd7873f7c0498a4de691919be6
rajneesh75 <rajneesh75@yahoo.com>
2025-11-26 22:34:33 +0530

    Merge remote-tracking branch 'origin/main'

--- pyspark1.py
+++ pyspark1.py
@@ -1,5 +1,7 @@
 from pyspark.sql import SparkSession
-spark = SparkSession.builder.master("local[*]").appName("test").getOrCreate()
+spark1 = SparkSession.builder.master("local[*]").appName("test1").getOrCreate()
+print(spark1.sparkContext)
+spark1.sparkContext.stop()
+spark2 = SparkSession.builder.master("local[*]").appName("test2").getOrCreate()
+print(spark2.sparkContext)
 
-df = spark.range(1, 10)
-df.show()
