e3477c552e67b52288b3d39f4365e3c4d5e34e0c
rajneesh75 <rajneesh75@yahoo.com>
2025-12-30 16:41:26 +0530

    Merge remote-tracking branch 'origin/main'

--- pyspark5.py
+++
@@ -1,6 +0,0 @@
-from pyspark.sql import SparkSession
-
-spark = SparkSession.builder.getOrCreate()
-spark.sparkContext.setLogLevel("INFO")
-df = spark.read.csv("customers-100.csv", header=True)
-print(df.show(10, truncate=False))
