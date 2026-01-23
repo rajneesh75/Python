c3e51e30fb29d3c136d6a28d5187be9068054b0b
rajneesh75 <rajneesh75@yahoo.com>
2026-01-23 21:15:14 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ databricks5.py
@@ -0,0 +1,5 @@
+from databricks.connect import DatabricksSession
+
+spark = DatabricksSession.builder.serverless().getOrCreate()
+df = spark.read.table("workspace.gold.customers_policies")
+df.show(10)
