5a4f92de148b0ada669d4b108cfa515888aee0e8
rajneesh75 <rajneesh75@yahoo.com>
2026-05-20 17:41:32 +0530

    commit

---
+++ langchain9.py
@@ -0,0 +1,90 @@
+from dotenv import load_dotenv
+import os, operator
+from langgraph.graph import StateGraph, END
+from typing import TypedDict, Annotated
+from langchain_core.messages import AnyMessage, SystemMessage, ToolMessage, HumanMessage
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+from langgraph.checkpoint.memory import MemorySaver
+
+load_dotenv()
+tavily_api_key = os.getenv("TAVILY_KEY")
+tool = TavilySearch(tavily_api_key=tavily_api_key)  # increased number of results
+
+
+class AgentState(TypedDict):
+    messages: Annotated[list[AnyMessage], operator.add]
+
+
+class Agent:
+
+    def __init__(self, model, tools, checkpointer, system=""):
+        print("Inside init")
+        self.system = system
+        graph = StateGraph(AgentState)
+        graph.add_node("llm", self.call_openai)
+        graph.add_node("action", self.take_action)
+        graph.add_conditional_edges(
+            "llm",
+            self.exists_action,
+            {True: "action", False: END}
+        )
+        graph.add_edge("action", "llm")
+        graph.set_entry_point("llm")
+        self.graph = graph.compile(checkpointer=checkpointer)
+        self.tools = {t.name: t for t in tools}
+        self.model = model.bind_tools(tools)
+        print("Exiting init")
+
+    def call_openai(self, state: AgentState):
+        print("Inside call_openai")
+        messages = state['messages']
+        if self.system:
+            messages = [SystemMessage(content=self.system)] + messages
+        message = self.model.invoke(messages)
+        print(messages)
+        return {'messages': [message]}
+
+    def exists_action(self, state: AgentState):
+        print("Inside exists_action")
+        result = state['messages'][-1]
+        print(f"Exiting exists_action {len(result.tool_calls)}")
+        return len(result.tool_calls) > 0
+
+    def take_action(self, state: AgentState):
+        print("Inside take_action")
+        tool_calls = state['messages'][-1].tool_calls
+        results = []
+        for t in tool_calls:
+            print(f"Calling: {t}")
+            if not t['name'] in self.tools:  # check for bad tool name from LLM
+                print("\n ....bad tool name....")
+                result = "bad tool name, retry"  # instruct LLM to retry if bad
+            else:
+                result = self.tools[t['name']].invoke(t['args'])
+            results.append(ToolMessage(tool_call_id=t['id'], name=t['name'], content=str(result)))
+        print(f"Back to the model with results: {results}")
+        return {'messages': results}
+
+
+prompt = """You are a smart research assistant. Use the search engine to look up information. \
+You are allowed to make multiple calls (either together or in sequence). \
+Only look up information when you are sure of what you want. \
+If you need to look up some information before asking a follow up question, you are allowed to do that!
+"""
+
+model = ChatOpenAI(model="gpt-4o")
+memory = MemorySaver()
+abot = Agent(model, [tool], system=prompt, checkpointer=memory)
+config = {"configurable": {"thread_id": "1"}}
+
+messages = [HumanMessage(content="Give me some cricket grounds located in Dwarka, Delhi. Give me urls to the grounds "
+                                 "if possible.")]
+print("Invoking graph...")
+result = abot.graph.invoke({"messages": messages}, config)
+print(f"final result: {result['messages'][-1].content}")
+
+messages = [HumanMessage(content="What about Jammu, Jammu & Kashmir?")]
+print("Invoking graph...")
+result = abot.graph.invoke({"messages": messages}, config)
+print(f"final result: {result['messages'][-1].content}")
