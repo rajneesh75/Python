35e3178adbe521bf512a299ff1d7a7f6310e2e2f
rajneesh75 <rajneesh75@yahoo.com>
2026-08-10 16:07:58 +0530

    commit

--- databricks5.py
+++ databricks5.py
@@ -1,11 +1,19 @@
 from databricks.connect import DatabricksSession
 import logging
 from dotenv import load_dotenv
+import os
 
 logging.basicConfig(level=logging.INFO)
 load_dotenv()
 
-spark = DatabricksSession.builder.serverless().getOrCreate()
-print(spark.version)
+spark = (
+    DatabricksSession.builder
+    .host("https://dbc-c379cba2-8489.cloud.databricks.com")
+    .token(str(os.getenv("DATABRICKS_TOKEN")))
+    .serverless(True)
+    .getOrCreate()
+)
+print("Spark Version:", spark.version)
+
 df = spark.read.table("workspace.bronze.products")
 df.show()
--- dqx1.py
+++ dqx1.py
@@ -3,9 +3,17 @@ from databricks.labs.dqx.profiler.profiler import DQProfiler
 from databricks.sdk import WorkspaceClient
 from databricks.labs.dqx.config import InputConfig
 import logging
+import os
 
 logging.basicConfig(level=logging.DEBUG)
-spark = DatabricksSession.builder.serverless().getOrCreate()
+spark = (
+    DatabricksSession.builder
+    .host("https://dbc-c379cba2-8489.cloud.databricks.com")
+    .token(os.getenv("DATABRICKS_TOKEN"))
+    .serverless(True)
+    .getOrCreate()
+)
+print("Spark Version:", spark.version)
 
 ws = WorkspaceClient()
 profiler = DQProfiler(ws)
